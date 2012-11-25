2403995141970655920c8664b1645cd8a8d2c26c
djordje <djordje.bigjoe@gmail.com>
2012-11-25 15:55:04 +0100

    Added `AccessDeniedException` copy from `li3_access`

---
+++ extensions/security/AccessDeniedException.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Lithium: the most rad php framework
+ *
+ * @copyright     Copyright 2011, Union of RAD (http://union-of-rad.org)
+ * @license       http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+namespace li3_usermanager\extensions\security;
+
+/**
+ * An `AccessDeniedException` is thrown whenever an unhandled attempt is made to access a restricted
+ * resource.
+ */
+class AccessDeniedException extends \RuntimeException {
+
+	protected $code = 403;
+
+}
+
+?>
