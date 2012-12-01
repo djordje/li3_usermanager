ab956e0df16e0e633d4c9f611d40a4fff8d9ea41
djordje <djordje.bigjoe@gmail.com>
2012-12-01 19:56:55 +0100

    Init login view added

---
+++ views/session/create.html.php
@@ -0,0 +1,14 @@
+<?php $this->title('Login'); ?>
+
+<?php if ($inactive): ?>
+	<p>Your account is inactive!</p>
+<?php endif; ?>
+
+<?=$this->form->create(null);?>
+	<?=$this->form->field('username');?>
+	<?=$this->form->field('password', array('type' => 'password'));?>
+	<?=$this->form->submit('Login', array('class' => 'btn btn-primary'));?>
+<?=$this->form->end();?>
+
+<p>Don't have account? You can register <?=$this->html->link('here', array('library' => 'li3_usermanager', 'Users::add'));?>.</p>
+<p>You can't remember your password? You can reset it <?=$this->html->link('here', array('library' => 'li3_usermanager', 'Users::requestResetPassword'));?>.</p>
