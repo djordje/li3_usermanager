59a45447c714a49e8086b74382f8a7b11fee7a6f
djordje <djordje.bigjoe@gmail.com>
2012-12-08 02:35:22 +0100

    New config constants and documentation

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -8,12 +8,18 @@ use lithium\security\Auth;
  * `RequireUserActivation` If true newly created users get mail with activation token, and account
  * stay inactive until activation.
  * `DefaultUserGroup` Default 2 (member), this is group that will be applied to all new users.
+ * `LI3_UM_TokenSalt` String that will be used as salt for generating activation and password
+ * reset tokens.
+ * `LI3_UM_ActivationEmailFrom` Email that'll be shown in email from for activation token
+ * `LI3_UM_PasswordResetEmailFrom` Email that'll be shown in email from for password reset token
+ * `LI3_UM_EnableUserRegistration` Boolean to enable control over user registration
  */
 define('LI3_UM_RequireUserActivation', true);
 define('LI3_UM_DefaultUserGroup', 2);
 define('LI3_UM_TokenSalt', 'AoJMd;sd5622');
 define('LI3_UM_ActivationEmailFrom', 'no-replay@example.com');
 define('LI3_UM_PasswordResetEmailFrom', LI3_UM_ActivationEmailFrom);
+define('LI3_UM_EnableUserRegistration', true);
 
 /**
  * Auth configurations
