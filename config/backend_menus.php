b509d944cf0e4586d18494dfdb5b552cbf51f964
djordje <djordje.bigjoe@gmail.com>
2013-05-22 00:51:14 +0200

    Initial `backend_menus` configuration for generating menus in `li3_backend`

---
+++ config/backend_menus.php
@@ -0,0 +1,10 @@
+<?php return array(
+	'component' => array(
+		'User manager' => array('li3_usermanager.ManageUsers::index', 'backend' => true)
+	),
+	'navigation' => array(
+		'li3_usermanager.ManageUsers' => array(
+			'Index' => array('li3_usermanager.ManageUsers::index', 'backend' => true)
+		)
+	)
+); ?>
